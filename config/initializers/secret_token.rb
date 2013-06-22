9fe71ef48cd9cde4bb6daa9644947a55c6d734f2
lingwu <agnovo_wl@hotmail.com>
2013-06-22 20:50:52 +0800

    20130621-TASK-A create an application
    仅创建了rails应用，product scaffold 及简单的页面设计

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ddc007696ca4668f0072208edf7365914c864e1697a7cb3cd964651e440e7a8f647097501f60b198f47e8f2a5f1162dcf1dfde69bb3b839121a22dadb1dc52ff'
