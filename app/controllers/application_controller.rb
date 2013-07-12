243eb615c0e3eb853d87ac19dfc6d314ae69be6e
lingwu <agnovo_wl@hotmail.com>
2013-07-13 00:06:19 +0800

    TASK-D 创建购物车
    1）session 管理
    2）模型 class 之间的关系
    3）button_to 按钮及 create 关系

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-end
+  
+  
+  # set the current_cart function private in the application controller ,
+  # so only the application controller can use this method
+  private
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart=Cart.create
+      session[:cart_id]=cart.id
+      cart
+    end
+end 
